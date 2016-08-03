c7438accc25ba0fa9f65def7b69cf55768be0424
Frank Hereford <frankhereford@users.noreply.github.com>
2016-08-03 12:43:55 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -62,7 +62,7 @@ foreach($CONFIG['term'] as $key => $s) {
 print "</ul>\n";
 
 #if ($CONFIG['graph_type'] == 'canvas') {
-if (0)
+if (0) {
 	chdir($CONFIG['webdir']);
 	include $CONFIG['webdir'].'/graph.php';
 } else {
